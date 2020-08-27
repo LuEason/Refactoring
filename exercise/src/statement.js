58b327de3c86bf3327961123162fcecf98894034
LuEason <chenzscd@163.com>
2020-08-27 14:51:11 +0800

    [Refactoring][Eason] refactor: extract method calculateTragedyAmount and calculateComedyAmount and format

--- exercise/src/statement.js
+++ exercise/src/statement.js
@@ -1,28 +1,39 @@
-function statement (invoice, plays) {
+const format = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+}).format;
+
+function calculateTragedyAmount(perf) {
+  let amount = 40000;
+  if (perf.audience > 30) {
+    amount += 1000 * (perf.audience - 30);
+  }
+  return amount;
+}
+
+function calculateComedyAmount(perf) {
+  let amount = 30000;
+  if (perf.audience > 20) {
+    amount += 10000 + 500 * (perf.audience - 20);
+  }
+  amount += 300 * perf.audience;
+  return amount;
+}
+
+function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
     switch (play.type) {
       case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
+        thisAmount = calculateTragedyAmount(perf);
         break;
       case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
+        thisAmount = calculateComedyAmount(perf);
         break;
       default:
         throw new Error(`unknown type: ${play.type}`);
